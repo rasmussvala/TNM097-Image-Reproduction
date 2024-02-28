a838010f7a3056cc05bace48d85b1acdacfca481
rasmussvala <rassv453@student.liu.se>
2024-02-28 20:16:03 +0100

    moved resize and db load to functions

--- matlab/create_poke_image.m
+++ matlab/create_poke_image.m
@@ -1,44 +1,19 @@
-function outputImage = create_poke_image(imagePath, databasePath)
+function outputImage = create_poke_image(img_path, db_path)
 
 BAR = waitbar(0,'Starting image reproduction...', 'Name', 'Image Reproduction');
 
-% Input image, the image which will be reproduced
-inputImage = imread(imagePath);
+% Read img and resize to proper size
+img = imread(img_path);
+img = resize_input_image(img);
 
-% Check if the input image is too small or too large
-if size(inputImage, 1) < 50 || size(inputImage, 2) < 50
-    warning('Input image is too small. Resizing to a larger size.');
-    inputImage = imresize(inputImage, 1.5);
-elseif size(inputImage, 1) > 500 || size(inputImage, 2) > 500
-    warning('Input image is too large. Resizing to a smaller size.');
-    inputImage = imresize(inputImage, 0.5);
-end
-
-% The data containing all of the smaller images
-fileList = dir(fullfile(databasePath, '*.jpg'));
-
-% The images are resized to blocksize x blocksize
-images = cell(1, numel(fileList));
-resized_images = cell(1, numel(fileList));
+% Load and preprocess database images
 blocksize = 32;
-
-% All the average colors from the data
-dataBase_avgColors = zeros(numel(fileList), 3);  % Matrix to store average colors (R, G, B)
-
-for i = 1:numel(fileList)
-    filename = fullfile(databasePath, fileList(i).name);
-    images{i} = im2double(imread(filename));
-    resized_images{i} = imresize(images{i}, [blocksize, blocksize]);
-
-    % Calculate average color for each channel (R, G, B)
-    dataBase_avgColors(i, 1) = mean(mean(resized_images{i}(:,:,1)));  % Red channel
-    dataBase_avgColors(i, 2) = mean(mean(resized_images{i}(:,:,2)));  % Green channel
-    dataBase_avgColors(i, 3) = mean(mean(resized_images{i}(:,:,3)));  % Blue channel
-end
+file_paths = dir(fullfile(db_path, '*.jpg'));
+[resized_db, db_avg_colors] = load_db(db_path, blocksize, file_paths);
 
 % Convert the input image to a matrix of RGB values
-inputImageSize = size(inputImage);
-inputImageMatrix = im2double(reshape(inputImage, [], 3));
+inputImageSize = size(img);
+inputImageMatrix = im2double(reshape(img, [], 3));
 
 % Perform k-means clustering to find the most common colors with increased iterations
 num_colors = 50;
@@ -46,7 +21,7 @@ opts = statset('MaxIter', 1000);  % Increase the maximum number of iterations
 [~, inputImage_commonColors] = kmeans(inputImageMatrix, num_colors, 'Options', opts);
 
 % Converts to LAB (Device independent format)
-dataBase_avgColors_LAB = rgb2lab(dataBase_avgColors);
+dataBase_avgColors_LAB = rgb2lab(db_avg_colors);
 
 % Converts to LAB (Device independent format)
 inputImage_commonColors_LAB = rgb2lab(inputImage_commonColors);
@@ -68,7 +43,7 @@ for colorIndex = 1:num_colors
     minMeanDeltaE = inf;
 
     % Iterate through each image in the DATABASE
-    for i = 1:numel(fileList)
+    for i = 1:numel(file_paths)
         % Extract LAB values of the current image from DATABASE
         databasePixelLab = dataBase_avgColors_LAB(i, :);
 
@@ -90,13 +65,13 @@ end
 selectedImagesIndices = unique(selectedImagesIndices);
 
 % Create a new matrix containing 50 unique images of Pokemon based on the selected indices
-selectedImages = resized_images(selectedImagesIndices);
+selectedImages = resized_db(selectedImagesIndices);
 
 % Iterate through each block in the OUTPUTIMAGE
 for row = 1:blocksize:outputImageSize(1)
     for col = 1:blocksize:outputImageSize(2)
         % Extract LAB values of the current block from INPUTIMAGE
-        inputBlockLab = rgb2lab(reshape(inputImage(ceil(row/blocksize), ceil(col/blocksize), :), 1, 1, 3));
+        inputBlockLab = rgb2lab(reshape(img(ceil(row/blocksize), ceil(col/blocksize), :), 1, 1, 3));
 
         % Initialize variables to store closest image information
         closestImageIdx = 0;
@@ -118,11 +93,39 @@ for row = 1:blocksize:outputImageSize(1)
         end
 
         % Replace the block in OUTPUTIMAGE with the corresponding block from the closest image
-        outputImage(row:(row+blocksize-1), col:(col+blocksize-1), :) = resized_images{closestImageIdx};
+        outputImage(row:(row+blocksize-1), col:(col+blocksize-1), :) = resized_db{closestImageIdx};
 
         waitbar(row / outputImageSize(1), BAR, sprintf('Reproduction: %.1f%%', row / outputImageSize(1) * 100));
     end
 end
 close(BAR);
 
+end
+
+% --------------- Functions ---------------
+
+function img = resize_input_image(img)
+if size(img, 1) < 50 || size(img, 2) < 50
+    warning('Input image is too small (under 50 pixels in height or width). Resizing to 150%.');
+    img = imresize(img, 1.5);
+elseif size(img, 1) > 500 || size(img, 2) > 500
+    warning('Input image is too large (over 500 pixels in height or width). Resizing to 50%.');
+    img = imresize(img, 0.5);
+end
+end
+
+function [resized_db, db_avg_colors] = load_db(db_path, blocksize, file_paths)
+num_files = numel(file_paths);
+resized_db = cell(1, num_files);
+db_avg_colors = zeros(num_files, 3);
+
+for i = 1:num_files
+    filename = fullfile(db_path, file_paths(i).name);
+    image = im2double(imread(filename));
+    resized_db{i} = imresize(image, [blocksize, blocksize]);
+
+    db_avg_colors(i, 1) = mean(mean(resized_db{i}(:,:,1)));
+    db_avg_colors(i, 2) = mean(mean(resized_db{i}(:,:,2)));
+    db_avg_colors(i, 3) = mean(mean(resized_db{i}(:,:,3)));
+end
 end
